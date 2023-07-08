8c003cf6300c1beeefe32167c83b33f9be2f51cb
IqbalsyahK <114941775+IqbalsyahK@users.noreply.github.com>
2023-07-08 11:59:51 +0700

    Add files via upload

---
+++ Aplikasi_Crop_Gambar_py/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, render_template, jsonify
+import cv2
+import numpy as np
+import base64
+import io
+
+app = Flask(__name__)
+
+def crop_by_position(img, position, size):
+    height, width = img.shape[:2]
+    if position == 'top_left':
+        return img[:size, :size]
+    elif position == 'top_center':
+        return img[:size, width // 2 - size // 2:width // 2 + size // 2]
+    elif position == 'top_right':
+        return img[:size, width - size:width]
+    elif position == 'center_left':
+        return img[height // 2 - size // 2:height // 2 + size // 2, :size]
+    elif position == 'center':
+        return img[height // 2 - size // 2:height // 2 + size // 2, width // 2 - size // 2:width // 2 + size // 2]
+    elif position == 'center_right':
+        return img[height // 2 - size // 2:height // 2 + size // 2, width - size:width]
+    elif position == 'bottom_left':
+        return img[height - size:height, :size]
+    elif position == 'bottom_center':
+        return img[height - size:height, width // 2 - size // 2:width // 2 + size // 2]
+    elif position == 'bottom_right':
+        return img[height - size:height, width - size:width]
+    else:
+        raise ValueError('Invalid position')
+
+def crop_image(img, position, size):
+    cropped_img = crop_by_position(img, position, size)
+    _, buffer = cv2.imencode('.jpg', cropped_img)
+    cropped_bytes = base64.b64encode(buffer)
+    return cropped_bytes
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'})
+
+    file = request.files['file']
+    position = request.form['position']
+    size = int(request.form['size'])
+
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'})
+
+    if file and allowed_file(file.filename):
+        img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+        cropped_bytes = crop_image(img, position, size)
+        return cropped_bytes
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
+
+if __name__ == '__main__':
+    app.run()
